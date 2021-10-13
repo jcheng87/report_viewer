7f89fe384f347e06ba887a713204fe4ea2df5ea5
Joe Cheng <jcheng1287@gmail.com>
2021-10-13 18:01:12 -0400

    initialize report_viewer file

---
+++ report_viewer.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd 
+
+def convert_report(df):
+    return df[columns_selected].to_csv(index=False)
+
+file_upload = st.file_uploader('Upload File', type='csv')
+
+
+if file_upload is not None:
+    data = pd.read_csv(file_upload)
+
+    data_columns = data.columns
+
+    columns_selected = st.multiselect("Column Names:",
+                                        data_columns,
+                                        )
+new_report = convert_report(data)
+
+st.download_button("Download", 
+                    data=new_report,
+                    file_name = 'report.csv'
+)
+
+
+
