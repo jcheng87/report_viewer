9e3be20c9bb77af7a7df16191bac26d01993cbf3
Joe Cheng <jcheng1287@gmail.com>
2021-10-13 20:58:37 -0400

    replaces dropdown list with checkbox

--- report_viewer.py
+++ report_viewer.py
@@ -1,32 +1,36 @@
 import streamlit as st
 import pandas as pd 
 
-def convert_report(df):
-    return df[columns_selected].to_csv(index=False)
+# filter df reports based on col_selected columns. 
+def convert_report(df, col_selected):
+    return df[col_selected].to_csv(index=False)
 
+# file upload 
 file_upload = st.file_uploader('Upload File', type='csv')
 
-
+# when file is uploaded, output list of columns to filter
 if file_upload is not None:
     data = pd.read_csv(file_upload)
 
     data_columns = data.columns
+    
+    col_checkbox_clicked = {}
 
-    columns_selected = st.multiselect("Column Names:",
-                                        data_columns,
-                                        )
+    for col in data_columns:
+        col_checkbox_clicked[col] = st.checkbox(col, key=col)
 
+    columns_selected = [col for col,checked_bool in col_checkbox_clicked.items() if checked_bool]
+
+# takes columns selected and runs new report
 run_report_button = st.button("Run Report")
-
 if run_report_button: 
-    new_report = convert_report(data)
+    new_report = convert_report(data,columns_selected)
 
-
-
+    # button to download new report
     st.download_button("Download", 
                         data=new_report,
                         file_name = 'report.csv'
-)
+                        )
 
 
 
